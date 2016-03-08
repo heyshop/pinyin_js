89b502d2214dc02116016c937fda4854fd27a4cc
liuhao <liuhao@yuhongtech.com>
2016-03-08 10:24:30 +0800

    add pinyin without yin

--- example/test.js
+++ example/test.js
@@ -2,7 +2,9 @@
  * Created by Alex on 2016/3/7.
  */
 var pinyin=require("../src/pinyin");
-pinyin.pinyin("你好");
+console.log(pinyin.pinyin("你好"));
 console.log(pinyin.isChineseWord("你好"));//true
 console.log(pinyin.isChineseWord("!你好"));//true
 console.log(pinyin.isChineseWord("!你好",true));//false
+//***没有音调的
+pinyin.pinyinWithOutYin("你好");
--- src/pinyin.js
+++ src/pinyin.js
@@ -2,6 +2,7 @@
  * Created by Alex on 2016/3/7.
  */
 var hzpy = require("./hanziPinyin").hzpy;
+var hzpyWithOutYin = require("./hanziPinyinWithoutYin").hzpy;
 exports.pinyin = function (word) {
     var str = '';
     var s;
@@ -10,6 +11,26 @@ exports.pinyin = function (word) {
             s = 1;
             while (hzpy.charAt(hzpy.indexOf(word.charAt(i)) + s) != ",") {
                 str += hzpy.charAt(hzpy.indexOf(word.charAt(i)) + s);
+                s++;
+            }
+            str += " ";
+        }
+        else {
+            str += word.charAt(i);
+        }
+    }
+    console.log(str);
+    return str;
+}
+//无声调的拼音
+exports.pinyinWithOutYin = function (word) {
+    var str = '';
+    var s;
+    for (var i = 0; i < word.length; i++) {
+        if (hzpyWithOutYin.indexOf(word.charAt(i)) != -1 && word.charCodeAt(i) > 200) {
+            s = 1;
+            while (hzpyWithOutYin.charAt(hzpyWithOutYin.indexOf(word.charAt(i)) + s) != ",") {
+                str += hzpyWithOutYin.charAt(hzpyWithOutYin.indexOf(word.charAt(i)) + s);
                 s++;
             }
             str += " ";
